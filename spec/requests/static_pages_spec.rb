4c01e8ee7d006fd319f69041ed3e525bb07bcccc
Sergey Smirnov <sergous@gmail.com>
2013-12-11 10:53:42 +0400

    RSpec links tests, test code refactoring

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
+shared_examples "StaticPage" do |h1, pageTitle|
+  it { should have_selector('h1', text: h1) }
+  it { should have_selector('title', text: full_title(pageTitle)) }
+end
+
+shared_examples "StaticLink" do |linkTitle, pageTitle|
+  it "should have the #{linkTitle} link on the layout" do
+    click_link linkTitle
+    page.should have_selector 'title', text: full_title(pageTitle)
+  end
+end
+
 describe "StaticPages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
@@ -8,30 +20,34 @@ describe "StaticPages" do
 
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "StaticPage", 'Sample App', ''
     it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    it_should_behave_like "StaticPage", 'Help', 'Help'
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    it_should_behave_like "StaticPage", 'About', 'About'
   end
 
   describe "Contact page" do
     before { visit contact_path }
-
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    it_should_behave_like "StaticPage", 'Contact', 'Contact'
   end
+
+  describe "LayoutLinks" do
+    before { visit root_path }
+
+    it_should_behave_like "StaticLink", 'Help', 'Help'
+    it_should_behave_like "StaticLink", 'About', 'About Us'
+    it_should_behave_like "StaticLink", 'Contact', 'Contact'
+    it_should_behave_like "StaticLink", 'Home', ''
+    it_should_behave_like "StaticLink", 'Sign up now!', 'Sign up'
+    it_should_behave_like "StaticLink", 'sample app', ''
+  end
+
 end
