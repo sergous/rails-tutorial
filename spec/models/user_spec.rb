95b6433c5289de4fbf65b4276937cec8e5ba13c3
Sergey Smirnov <sergous@gmail.com>
2013-12-11 19:18:40 +0400

    Add validation and authentication for User model with RSpec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,16 @@ describe User do
 
   it { should be_valid }
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be save as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when name is not present" do
     before { @user.name = "" }
     it { should_not be_valid }
