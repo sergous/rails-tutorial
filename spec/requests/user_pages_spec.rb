2d4f4fef8f16ef705c632c1129c4ecd571a371f0
Sergey Smirnov <sergous@gmail.com>
2013-12-15 19:04:30 +0400

    SignIn Form and RSpec refactoring

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,17 +1,5 @@
 require 'spec_helper'
 
-shared_examples "SignupError" do |error, name, email, password, confirmation|
-  before do
-    fill_in "Name",           with: name
-    fill_in "Email",          with: email
-    fill_in "Password",       with: password
-    fill_in "Confirmation",   with: confirmation
-    click_button submit
-  end
-  it { should have_selector('title', text: 'Sign up') }
-  it { should have_content(error) }
-end
-
 describe "UserPages" do
 
   subject { page }
@@ -35,13 +23,48 @@ describe "UserPages" do
     end
 
     describe "after submission" do
-      it_should_behave_like "SignupError", "6 errors"
-      it_should_behave_like "SignupError", "Name can't be blank", "", "user@example.com", "foobar", "foobar"
-      it_should_behave_like "SignupError", "Email can't be blank", "Example User", "", "foobar", "foobar"
-      it_should_behave_like "SignupError", "Email is invalid", "Example User", "user@example", "foobar", "foobar"
-      it_should_behave_like "SignupError", "Password can't be blank", "Example User", "user@example.com", "", "foobar"
-      it_should_behave_like "SignupError", "Password is too short (minimum is 6 characters)", "Example User", "user@example.com", "foo", "foobar"
-      it_should_behave_like "SignupError", "Password confirmation can't be blank", "Example User", "user@example.com", "foobar", ""
+
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "with all blank fields" do
+        before { signup('', '', '', '') }
+        it { should have_signup_error("6 errors") }
+      end
+
+      describe "with blank name field" do
+        before { signup('', user.email, user.password, user.password_confirmation) }
+        it { should have_signup_error("Name can't be blank") }
+      end
+
+      describe "with blank email field" do
+        before { signup(user.name, '', user.password, user.password_confirmation) }
+        it { should have_signup_error("Email can't be blank") }
+      end
+
+      describe "with wrong email field" do
+        before { signup(user.name, 'user@example', user.password, user.password_confirmation) }
+        it { should have_signup_error("Email is invalid") }
+      end
+
+      describe "with blank password field" do
+        before { signup(user.name, user.email, '', user.password_confirmation) }
+        it { should have_signup_error("Password can't be blank") }
+      end
+
+      describe "with short password field" do
+        before { signup(user.name, user.email, '12345', user.password_confirmation) }
+        it { should have_signup_error("Password is too short (minimum is 6 characters)") }
+      end
+
+      describe "with blank password confirmation field" do
+        before { signup(user.name, user.email, user.password, '') }
+        it { should have_signup_error("Password confirmation can't be blank") }
+      end
+
+      describe "with different password fields" do
+        before { signup(user.name, user.email, user.password, 'foobare') }
+        it { should have_signup_error("Password doesn't match confirmation") }
+      end
     end
 
     describe "with valid information" do
@@ -52,6 +75,11 @@ describe "UserPages" do
         fill_in "Confirmation",   with: "foobar"
       end
 
+    # TODO:
+    # let(:user) { FactoryGirl.create(:user) }
+    #
+    #  before { valid_signup(user) }
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
@@ -61,7 +89,7 @@ describe "UserPages" do
         let(:user) { User.find_by_email("user@example.com") }
 
         it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
         it { should have_link('Sign out') }
       end
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,41 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",      with: user.email
+  fill_in "Password",   with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+def signup(name, email, password, confirmation)
+    fill_in "Name",           with: name
+    fill_in "Email",          with: email
+    fill_in "Password",       with: password
+    fill_in "Confirmation",   with: confirmation
+    click_button submit
+end
+
+#def valid_signup(user)
+#  fill_in "Name",           with: user.name
+#  fill_in "Email",          with: user.email
+#  fill_in "Password",       with: user.password
+#  fill_in "Confirmation",   with: user.password_confirmation
+#end
+
+RSpec::Matchers.define :have_signup_error do |message|
+  match do |page|
+    page.should have_selector('title', text: 'Sign up')
+    page.should have_content(message)
+  end
+end
