fa182adcec797b70e94b4c5def5660f060870113
Sergey Smirnov <sergous@gmail.com>
2013-12-24 18:20:29 +0400

    Finish Ch10 homework

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -31,6 +31,31 @@ describe "Micropost pages" do
 
   end
 
+  describe "microposts list" do
+    let(:num) {50}
+    before(:all) { num.times { FactoryGirl.create(:micropost, user: user) } }
+    after(:all) { User.delete_all }
+
+    describe "counter" do
+      let(:counter) {user.microposts.count.to_s + " microposts"}
+      before { visit root_path }
+
+      it { page.should have_selector('span', text: counter ) }
+      it { expect(page).to have_content('micropost'.pluralize(user.microposts.count)) }
+    end
+
+    describe "pagination" do
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each micropost" do
+        Micropost.paginate(page: 1).each do |micropost|
+          page.should have_selector('li', text: micropost.content)
+        end
+      end
+    end
+  end
+
   describe "micropost destruction" do
     before { FactoryGirl.create(:micropost, user: user) }
 
@@ -39,6 +64,20 @@ describe "Micropost pages" do
 
       it "should delete a micropost" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+
+    end
+
+    describe "as incorrect user" do
+      let(:user2) { FactoryGirl.create(:user) }
+
+      before {
+        sign_in user2
+        visit root_path
+      }
+
+      it "should not delete a micropost" do
+        should_not have_link('delete')
       end
 
     end
